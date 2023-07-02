6ba7b92f7799b26be7ba4cfe4b3da5f5df0e1f56
Jan Heil <office@byte-store.de>
2023-07-02 14:30:57 +0200

    Added working Main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,44 @@
 #include <Arduino.h>
-#include "HomeAssistant.h"
+
+// Webserver and WebSerial
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSerial.h>
+#include <ArduinoHA.h>
+#include <WiFiClient.h>
+
+// Define Network Credentials
+const char *ssid = "HRouter-1";
+const char *password = "13280073116925758533";
+
+// Define WiFi Client.
+WiFiClient client;
+
+// Define Home Assistant Credentials.
+HADevice device;
+
+// Define Home Assistant MQTT Instance.
+HAMqtt mqtt(client, device);
+
+// Store Volume Slider.
+HANumber volume("Volume");
+
+// Store Input Select.
+HASelect input("Input");
+
+// Store Mute Switch.
+HASwitch mute("Mute");
+
+// Store Standby Switch.
+HASwitch standby("Standby");
+
+// Store Power Switch.
+HASwitch power("Power");
+
+// Store Bus Version.
+HASelect version("Version");
+
 /**
  * Bread Board:
  * -----------------
@@ -10,7 +49,7 @@
  */
 
 // Define new Server Object.
-//AsyncWebServer server(80);
+AsyncWebServer server(80);
 
 /*
 Kenwood XS/XS8/SL16 system remote control
@@ -98,7 +137,76 @@ void onConnected();
 void onMessage(const char *topic, const uint8_t *payload, uint16_t length);
 
 void setup() {
-    prepare();
+    // Begin Serial.
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+
+    // Connect to Wi-Fi Network.
+    WiFi.begin(ssid, password);
+
+    // Wait until Device is Connected.
+    while (WiFi.status() != WL_CONNECTED) {
+        // Print Waiting Point.
+        Serial.print(".");
+
+        // Let Status LED Blink.
+        status_led(150);
+    }
+
+    // Print Success Message.
+    Serial.println("Connected to WiFi.");
+
+    // Get MAC Address into Buffer.
+    byte mac[WL_MAC_ADDR_LENGTH];
+    WiFi.macAddress(mac);
+
+    // Set ID of Device.
+    device.setUniqueId(mac, sizeof(mac));
+
+    // Prepare for Home Assistant.
+    device.setName("Energy Switch");
+    device.setSoftwareVersion("1.0.0");
+    device.setModel("ESP8266");
+    device.setManufacturer("Jan Heil (www.byte-store.de)");
+    device.enableSharedAvailability();
+    device.enableLastWill();
+
+    // Prepare Volume Slider.
+    volume.setMax(100);
+    volume.setMin(0);
+    volume.setName("Volume");
+    volume.setIcon("mdi:volume-medium");
+    volume.setMode(HANumber::ModeSlider);
+
+    // Prepare Mute Switch.
+    mute.setName("Mute");
+    mute.setIcon("mdi:volume-off");
+
+    // Prepare Standby Switch.
+    standby.setName("Standby");
+    standby.setIcon("mdi:power-sleep");
+
+    // Prepare Power Switch.
+    power.setName("Power");
+    power.setIcon("mdi:power");
+
+    // Prepare Input Select.
+    input.setName("Input");
+    input.setIcon("mdi:knob");
+    input.setOptions("TV;Phono");
+
+    // Prepare BUS Version.
+    version.setIcon("mdi:cable-data");
+    version.setName("BUS-Version");
+    version.setOptions("XS8;SL16");
+    version.setCurrentState((interface == 8 ? 0 : 1));
+
+    // Add MQTT Listener.
+    mqtt.onMessage(onMessage);
+    mqtt.onConnected(onConnected);
+
+    // Connect to HomeAssistant.
+    mqtt.begin("homeassistant.local", "energyswitch", "energyswitch");
 
     // Set Input and Output Pins.
     //setPins();
@@ -108,6 +216,17 @@ void setup() {
 
     // Print Info Commands.
     print_info();
+}
+
+void onConnected() {
+    println("Connected to MQTT Server.");
+}
+
+void onMessage(const char *topic, const uint8_t *payload, uint16_t length) {
+    print("Received ");
+    print(topic);
+    println(" - ");
+    //(payload);
 }
 
 /**
